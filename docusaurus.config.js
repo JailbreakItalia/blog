c4d83c2a05af9ff9a150b77bad9c805a390d1ffd
basketshoe <54729294+basketshoe@users.noreply.github.com>
2025-12-04 20:12:39 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,9 +70,9 @@ const config = {
       colorMode: {
         respectPrefersColorScheme: true,
       },
-      metadata:[
+      metadata: {
         {name: 'keywords', content: 'jailbreak, Italia, iPhone, mod, modding, iPad, iOS, iPadOS, telegram'}
-      ],
+      },
       navbar: {
         title: ' Italia',
         logo: {
